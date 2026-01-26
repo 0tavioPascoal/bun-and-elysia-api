487dcf5ce1d88cb5372e4b724b3749df3307aeb9
OtavioPascoal <fjoooosa@gmail.com>
2026-01-25 23:48:08 -0300

    feat: add route and validate for cadaster restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,33 @@
-import Elysia from 'elysia'
+import {Elysia, t} from 'elysia'
 import { env } from '../env'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World!'
+const app = new Elysia().post('/restaurants', async ({body, set}) => {
+  const {restaurantName, managerName, email,phone} = body
+
+  const [manager] = await db.insert(users).values({
+    name: managerName,
+    email,
+    phone,
+    role: 'manager'
+  }).returning({
+    id: users.id
+  })
+
+  await db.insert(restaurants).values({
+    name: restaurantName,
+    managerId: manager?.id,
+  })
+
+  return set.status = 204
+}, {
+  body: t.Object({
+    managerName: t.String(),
+    restaurantName: t.String(),
+    phone: t.String(),
+    email: t.String({format: 'email'})
+  })
 })
 
 app.listen(env.PORT, () => {
